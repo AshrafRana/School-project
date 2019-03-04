d1398d01f5bdb909ad3d322638553ffe53cf19ea
AshrafRana <AshrafRana>
2019-03-04 11:52:36 +0500

    setup working condition with sql mdf 3-4-2019

--- School_Managment_System/Classes/Singleton.cs
+++ School_Managment_System/Classes/Singleton.cs
@@ -17,14 +17,8 @@ namespace School_Managment_System
             if (connection == null)
             {
 
-                //"Data Source=.\SQLEXPRESS;AttachDbFilename=filepath\bin\Debug\MyDB.mdf;Integrated Security=True;Connect Timeout=30;User Instance=True"
-
-
-              //  connection = new SqlConnection(@"Data Source=.\SQLEXPRESS;AttachDbFilename=|DataDirectory|\sms.mdf;Integrated Security=True;");
-
-                connection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\sms.mdf;Integrated Security=True;");
-
-                //connection = new SqlConnection("Data Source=DESKTOP-PTH0QEP\\SQLEXPRESS; Initial Catalog=sms;Integrated Security=True");
+                
+                connection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Data\sms.mdf;Integrated Security=True;");
                 
                 return connection;
 
